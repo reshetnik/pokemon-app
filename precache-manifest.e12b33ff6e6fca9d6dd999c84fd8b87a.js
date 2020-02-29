0797dc8c84ec096102305aa5ebfdc305521d9917
Alexander Reshetnik <reshetnikvsb@gmail.com>
2020-02-29 10:07:35 +0300

    Updates

---
+++ precache-manifest.e12b33ff6e6fca9d6dd999c84fd8b87a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d65936ba6ee83deb1f1ce0f163ecd675",
+    "url": "/pokemon-app/index.html"
+  },
+  {
+    "revision": "56d7fbd7dafe021de4e5",
+    "url": "/pokemon-app/static/css/main.1e22f64a.chunk.css"
+  },
+  {
+    "revision": "1c9eef9e29cc1682e8b6",
+    "url": "/pokemon-app/static/js/2.d4feda02.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/pokemon-app/static/js/2.d4feda02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56d7fbd7dafe021de4e5",
+    "url": "/pokemon-app/static/js/main.d31b13ce.chunk.js"
+  },
+  {
+    "revision": "d694666e359bcb577377",
+    "url": "/pokemon-app/static/js/runtime-main.21145d5d.js"
+  }
+]);
